8f9f7bed336fd60da351e7e11cc1598eb9dff3f1
jpl88 <jpl88@case.edu>
2016-11-07 11:03:37 -0500

    Added MessageHandler run method
    Signed-off-by: jpl88 <jpl88@case.edu>

--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,9 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.HashSet;
 
 /**
@@ -10,20 +14,86 @@ public class Server {
 	
 	private static final int PORT = 500026;
 	
-	private static HashSet<String> names = new HashSet<String>();
+	private static HashSet<String> usernames = new HashSet<String>();
 	
 	private static HashSet<PrintWriter> writers = new HashSet<PrintWriter>();
 	
 	public static void main(String[] args){
-		System.out.println("Chat server is now running");
-		ServerSocket listener = new ServerSocket(PORT);
 		try{
+			System.out.println("Chat server is now running");
+			ServerSocket listener = new ServerSocket(PORT);
 			while(true){
 				new MessageHandler(listener.accept()).start();
 			}
+		}
+		catch(IOException e){
+			throw new IOException(e.getMessage());
 		}
 		finally{
 			listener.close();
 		}
 	}
-}
+	
+	private static class MessageHandler extends Thread{
+		
+		private String username;
+		private Socket socket;
+		private BufferedReader inputReader;
+		private PrintWriter outputWriter;
+		
+		public MessageHandler(Socket socket){
+			this.socket = socket;
+		}
+		
+		public void run(){
+			try{
+				inputReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				outputWriter = new PrintWriter(socket.getOutputStream(), true);
+				while (true) {
+					outputWriter.println("Attempting to submit username");
+	                username = inputReader.readLine();
+	                if (username == null) {
+	                    throw new NullPointerException();
+	                }
+	                synchronized (usernames) {
+	                    if (!usernames.contains(username)) {
+	                        usernames.add(username);
+	                        break;
+	                    }
+	                }
+				}
+				outputWriter.println("NAMEACCEPTED");
+                writers.add(outputWriter);
+                while (true) {
+                    String input = inputReader.readLine();
+                    if (input == null) {
+                        throw new NullPointerException();
+                    }
+                    for (PrintWriter writer : writers) {
+                        writer.println("MESSAGE " + username + ": " + input);
+                    }
+                }
+			}
+			catch(NullPointerException e){
+				System.out.println(e.getMessage());
+			}
+			catch(IOException e){
+				System.out.println(e.getMessage());
+			}
+			finally{
+				if (username != null) {
+                    usernames.remove(username);
+                }
+                if (outputWriter != null) {
+                    writers.remove(outputWriter);
+                }
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                }
+			}
+		}
+		
+	}  //end class MessageHandler
+
+}  //end class Server
