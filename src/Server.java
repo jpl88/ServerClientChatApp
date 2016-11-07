3e597d5da37b1b3b62787b6929a6c379cacd250e
jpl88 <jpl88@case.edu>
2016-11-07 10:41:07 -0500

    began server logic & added MessageHandler class.
    Signed-off-by: jpl88 <jpl88@case.edu>

--- src/Server.java
+++ src/Server.java
@@ -1,8 +1,29 @@
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.util.HashSet;
+
 /**
  * 
  * @author justinlee
- *
  */
 public class Server {
-
+	
+	private static final int PORT = 500026;
+	
+	private static HashSet<String> names = new HashSet<String>();
+	
+	private static HashSet<PrintWriter> writers = new HashSet<PrintWriter>();
+	
+	public static void main(String[] args){
+		System.out.println("Chat server is now running");
+		ServerSocket listener = new ServerSocket(PORT);
+		try{
+			while(true){
+				new MessageHandler(listener.accept()).start();
+			}
+		}
+		finally{
+			listener.close();
+		}
+	}
 }
